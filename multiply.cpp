68aa5f7386df65e1770d1a3c3eafc35a16e38226
Alex151821 <dennisadrew@icloud.com>
2019-11-17 10:41:16 -0700

    Added files to git. Includes multiply function and main program.

---
+++ multiply.cpp
@@ -0,0 +1,54 @@
+/*
+ * multiply.cpp
+ *
+ *  Created on: Nov 17, 2019
+ *      Author: denni
+ */
+#include <vector>
+#include <iostream>
+#include "multiply.h"
+
+using namespace std;
+
+vector<vector<int> > multiply(vector<vector<int> > a, vector<vector<int> > b){
+  vector<vector<int> > c(a.size());
+  int total = 0;
+
+  for (int arow = 0; arow < a.size(); arow++){
+    int bcol = 0;
+    int acol = 0;
+    //cout << "\n";
+    //cout << "\n";
+
+    for (int bcol = 0; bcol < b[0].size(); bcol ++){
+      for (int brow = 0; brow < b.size(); brow ++){
+        //cout << b[brow][bcol] << " ";
+        //cout << a[arow][acol] << "\n";
+
+        total += (a[arow][acol]* b[brow][bcol]);
+        //cout << total << "\n";
+        acol ++;
+
+      }
+    c[arow].push_back(total);
+    total = 0;
+    acol = 0;
+
+    }
+  }
+
+
+
+//cout << "\n";
+/*
+  for (vector<int> i : c){
+    for (int j : i){
+      cout << j << " ";
+    }
+    cout << "\n";
+  }*/
+  return c;
+}
+
+
+
---
+++ multiply.h
@@ -0,0 +1,18 @@
+/*
+ * multiply.h
+ *
+ *  Created on: Nov 17, 2019
+ *      Author: denni
+ */
+
+#ifndef MULTIPLY_H_
+#define MULTIPLY_H_
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int> > multiply(vector<vector<int> > a, vector<vector<int> > b);
+
+
+
+#endif /* MULTIPLY_H_ */
