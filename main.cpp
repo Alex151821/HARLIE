2e195d53b9e0c48e7104a03109d911596fa1c563
Alex151821 <dennisadrew@icloud.com>
2019-11-19 02:56:39 +0000

    Added audio file reader, added main to .gitignore, tested AudioFile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "AudioFile/AudioFile.h" 
 using namespace std;
 int main(){
-  cout << "hello world\n";
+  AudioFile<double> boom;
+  boom.load("Boomdeyada.wav");
+  cout << "Sample rate: " << boom.getSampleRate();
+  int numSamples = boom.getNumSamplesPerChannel();
+  int channel = 0;
+
+  for (int i = 0; i < numSamples; i++){
+      cout << boom.samples[channel][i];
+  }
   return 0;
 }
