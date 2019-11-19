1b6a7c3cbe352faae4dda6abdd8e0478183fd30a
Alex151821 <dennisadrew@icloud.com>
2019-11-19 03:07:57 +0000

    AudioFile test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "AudioFile/AudioFile.h" 
+#include <vector>
 using namespace std;
 int main(){
-  AudioFile<double> boom;
-  boom.load("Boomdeyada.wav");
-  cout << "Sample rate: " << boom.getSampleRate();
-  int numSamples = boom.getNumSamplesPerChannel();
-  int channel = 0;
-
-  for (int i = 0; i < numSamples; i++){
-      cout << boom.samples[channel][i];
-  }
-  return 0;
+    AudioFile<double> boom;
+    boom.load("Boomdeyada.wav");
+    cout << "Sample rate: " << boom.getSampleRate();
+    int numSamples = boom.getNumSamplesPerChannel();
+    int channel = 0;
+    vector<double> samples;
+    for (int i = 0; i < numSamples; i++){
+      cout << boom.samples[channel][i] << ", ";
+    }
+    cout << "\n";
+    return 0;
 }
