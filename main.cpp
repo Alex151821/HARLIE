fdf43b7ad4ab1c50e629b998a6a7702d87fbe139
Alex151821 <dennisadrew@icloud.com>
2019-11-20 15:32:41 +0000

    Added library credits

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@ using namespace std;
 int main(){
     AudioFile<double> boom;
     boom.load("Boomdeyada.wav");
-    cout << "Sample rate: " << boom.getSampleRate();
+    cout << "Sample rate: " << boom.getSampleRate() << "\n";
     int numSamples = boom.getNumSamplesPerChannel();
     int channel = 0;
+    cout << numSamples << "\n";
     vector<double> samples;
-    for (int i = 0; i < numSamples; i++){
-      cout << boom.samples[channel][i] << ", ";
+    double SampArr[320];
+    for (int i = 0; i < 320; i++){
+        SampArr[i] = boom.samples[channel][i];
+    }
+    for(double r: SampArr){
+        cout << r << ", ";
     }
     cout << "\n";
     return 0;
