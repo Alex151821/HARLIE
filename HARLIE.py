c2e979b41216a16e829d5fb8b51a31fcfee84b38
Alex151821 <dennisadrew@icloud.com>
2019-12-09 20:35:59 +0000

    added dice roll functionality

--- HARLIE.py
+++ HARLIE.py
@@ -20,6 +20,7 @@ async def on_message(message):
     if message.content.startswith(":H"):
       await message.channel.send('You Got me')
       message_content = message.content.split(' ')
+      print(message_content[2])
       if message_content[1] == 'say':
         await sayFunction(message_content, message)
       elif message_content[1] == 'roll':
@@ -48,7 +49,45 @@ async def sayFunction(message_content, message):
 
 @client.event
 async def rollFunction(message_content, message):
-    return 0
+    a = message_content.split("d")
+    b = []
+    final = []
+    a[0] = int(a[0])
+    a[2] = int(a[2])
+    for i in range(0, a[0]):
+      for j in range(0, 4):
+        b.append(random.randint(0, a[2]))
+      final.append(b[random.randint(0, 5)])
+      
+    
+
+    """
+    a = []
+    b = []
+    final = []
+    for i in message_content[2]:
+      a.append(i)
+    try:
+      if a[3]:
+        a[2] = int(a[2])
+        a[2] = a[2]*10
+        a[2] = a[2] + int(a[3])
+        a.pop(3)
+    except:
+      pass
+      
+
+    for i in range(0, int(a[0])):
+      for i in range(0, 5):
+        b.append(random.randint(0, int(a[2])))
+      final.append(str(b[random.randint(0, 4)]))
+    """
+    
+    await message.channel.send("Final results for " + message_content[2] +": " + " ".join(final))
+
+
+
+    
 
 keep_alive()
 token = os.environ.get('DISCORD_BOT_SECRET')
