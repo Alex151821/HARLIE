c4e5cf6a5655ca0c05095b0d4d433809c5ceaab9
Alex151821 <dennisadrew@icloud.com>
2019-12-19 14:59:13 -0700

    Fixed rpg dice roll

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-import os
-print(os.getenv('DISCORD_BOT_SECRET'))
+import random
+
+message_content = [':H', "roll", "1d6"]
+a = message_content[2].split("d")
+print (a)
+b = []
+final = []
+a[0] = int(a[0])
+a[1] = int(a[1])
+for i in range(0, a[0]):
+    for j in range(0, 5):
+        b.append(random.randint(0, a[1]))
+    print(b)
+    final.append(b[random.randint(0, 4)])
+    print (final)
+print("Final results for " + message_content[2] +": " + " ".join(map(str, final)))
