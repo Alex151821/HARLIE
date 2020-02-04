1b5601e59b0c3813e0f802c690d63ace71bd9e1c
Alex151821 <dennisadrew@icloud.com>
2020-02-03 23:40:50 -0700

    added plotting to preprocess, added test audio

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,15 @@
 from scipy.io import wavfile
 from scipy.fftpack import fft
 from matplotlib import pyplot as plt
-print(wavfile.read("Boomdeyada.wav"))
-sf, data = wavfile.read("Boomdeyada.wav")
-a = data[:319].T[0]
-b = [(n/2**16.) * 2-1 for n in a]
-print(b[:500])
+print(wavfile.read("New Recording 6 trimmed.wav"))
+sf, data = wavfile.read("New Recording 6 trimmed.wav")
+
+a = data.T[:319]
+b = [(n/2**16.) * 2-1 for n in data]
+print(b[:20])
+
 c = fft(b)
 d = len(c)/2
-<<<<<<< HEAD
 #print(c[:100])
-
-=======
-#print(c[:100])
->>>>>>> 7c7e099d4709b2e2ef813635857d8cf186f34d87
+plt.specgram(b, Fs=sf, NFFT = 320)
+plt.show()
