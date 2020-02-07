29772d94ec07f1d735d74bc6baa16c9393cf32e8
Alex151821 <dennisadrew@icloud.com>
2020-02-07 19:12:51 +0000

    h

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-import random
-
-message_content = [':H', "roll", "1d6"]
-a = message_content[2].split("d")
-print (a)
-b = []
-final = []
-a[0] = int(a[0])
-a[1] = int(a[1])
-for i in range(0, a[0]):
-    for j in range(0, 5):
-        b.append(random.randint(0, a[1]))
-    print(b)
-    final.append(b[random.randint(0, 4)])
-    print (final)
-print("Final results for " + message_content[2] +": " + " ".join(map(str, final)))
+i = 0
+while i < 5:
+    i += 1
+    print(i)
